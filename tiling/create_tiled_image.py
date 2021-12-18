6fb9aeef72d0fb73e8b52452b83d8767d3af31ef
MiloCS <miloccs@gmail.com>
2021-12-18 15:39:17 -0500

    tiling working

--- tiling/create_tiled_image.py
+++ tiling/create_tiled_image.py
@@ -1,5 +1,35 @@
-import numpy
+import numpy as np
+import sys
+from load_images import load_images, SECTION_SIZE, load_base_image
+from PIL import Image
 
-if __name__ == '__main___':
-	image_to_tile = sys.argv[1]
-	tiling_images_folder = sys.argv[2]
+np.set_printoptions(threshold=sys.maxsize)
+
+def construct_tiling(imgs, base):
+	print(imgs.shape, base.shape)
+	tiled_img = np.zeros(base.shape)
+	baseh = base.shape[0]
+	basew = base.shape[1]
+	iterh = baseh // SECTION_SIZE
+	iterw = basew // SECTION_SIZE
+	for i in range(iterh):
+		for j in range(iterw):
+			tempim = base[i*SECTION_SIZE:(i+1)*SECTION_SIZE,j*SECTION_SIZE:(j+1)*SECTION_SIZE]
+
+			diffs = np.abs(imgs - tempim).sum(axis=(1, 2, 3))
+			idxs = np.argsort(diffs)[0:3]
+			idx = np.random.choice(idxs)
+			tiled_img[i*SECTION_SIZE:(i+1)*SECTION_SIZE,j*SECTION_SIZE:(j+1)*SECTION_SIZE] = imgs[idx]
+
+	return tiled_img
+
+
+image_to_tile = sys.argv[1]
+tiling_images_folder = sys.argv[2]
+i = load_images(tiling_images_folder)
+b = load_base_image(image_to_tile)
+
+c = construct_tiling(i, b)
+result = Image.fromarray(np.uint8(c)).convert('RGB')
+result.show()
+result.save(image_to_tile.split(".")[0] + "_tiled.jpg")
--- tiling/load_images.py
+++ tiling/load_images.py
@@ -1,5 +1,38 @@
-import Pillow, sys, cv2, numpy
+import os
+from PIL import Image, ImageOps
+import numpy as np
 
+SECTION_SIZE = 48
+
+
+def crop_center(img):
+	w, h = img.size
+	min_wh = w if w < h else h
+	return img.crop((0, 0, min_wh, min_wh))
 
 def load_images(tiling_images_folder):
-	pass
+	imgs = []
+	size = SECTION_SIZE, SECTION_SIZE
+
+	for file in os.listdir(tiling_images_folder):
+		with Image.open(tiling_images_folder + "/" + file) as im:
+			if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+				continue
+
+			im = crop_center(im)
+			im.thumbnail(size)
+
+			#adding to numpy array
+			a = np.array(im)
+			imgs.append(a)
+
+	return np.array(imgs).astype(np.int8)
+
+def load_base_image(filename):
+	with Image.open(filename) as im:
+		w, h = im.size
+		neww = int(w / SECTION_SIZE) * SECTION_SIZE
+		newh = int(h / SECTION_SIZE) * SECTION_SIZE
+		im = im.resize((neww, newh))
+		a = np.array(im).astype(np.int8)
+	return a
